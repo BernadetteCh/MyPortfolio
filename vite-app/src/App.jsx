cbb48ec0956cad075d252f64407629a3128f3579
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-06 22:26:27 +0200

    Add main fonts and colors

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
-import Button from "react-bootstrap/Button";
 
 function App() {
-  return (
-    <div class="container py-4 px-3 mx-auto">
-      <h1>Hi</h1>
-      <Button variant="primary">Primary</Button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
