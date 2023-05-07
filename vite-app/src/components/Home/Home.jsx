9c9558edcab6846ce7c191699342c3facb167d56
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-07 14:52:06 +0200

    create header

--- vite-app/src/components/Home/Home.jsx
+++ vite-app/src/components/Home/Home.jsx
@@ -1,5 +1,27 @@
+import "../../components/Home/Home.css";
+import header from "../../img/header.png";
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <div>Hi from home</div>;
+  return (
+    <div>
+      <img src={header}></img>
+
+      <div style={{ textAlign: "center" }}>
+        <Link
+          to="/contact"
+          style={{
+            textDecoration: "none",
+            display: "block",
+            margin: "0px auto",
+            fontSize: "20px",
+          }}
+        >
+          <button>Contact me</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
