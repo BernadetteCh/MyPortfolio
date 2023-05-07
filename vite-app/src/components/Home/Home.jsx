f5c0a3133b08da23247b1d672c60ec60cc9ae865
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-07 15:35:24 +0200

    create home page

--- vite-app/src/components/Home/Home.jsx
+++ vite-app/src/components/Home/Home.jsx
@@ -1,5 +1,8 @@
 import "../../components/Home/Home.css";
 import header from "../../img/header.png";
+import weatherapp from "../../img/weatherapp.png";
+import dictionaryapp from "../../img/dictionaryapp.png";
+import employeemadness from "../../img/employeemadness.png";
 import { Link } from "react-router-dom";
 
 function Home() {
@@ -20,6 +23,50 @@ function Home() {
           <button>Contact me</button>
         </Link>
       </div>
+      <h2 style={{ fontWeight: "lighter", marginTop: "50px" }}>
+        LATEST PROJECTS ...
+      </h2>
+      <div className="container text-center">
+        <div className="row justify-content-center projectimages">
+          <div className="col">
+            <a
+              href="https://optimistic-curran-8bd91f.netlify.app/"
+              target="_blank"
+            >
+              {" "}
+              <img src={weatherapp}></img>
+            </a>
+          </div>
+          <div className="col">
+            <a
+              href="https://frosty-lumiere-3bb161.netlify.app/"
+              target="_blank"
+            >
+              {" "}
+              <img src={dictionaryapp}></img>
+            </a>
+          </div>
+          <div className="col">
+            <a
+              href="https://github.com/BernadetteCh/my-employees-data"
+              target="_blank"
+            >
+              <img src={employeemadness}></img>
+            </a>
+          </div>
+        </div>
+      </div>
+      <Link
+        to="/projects"
+        style={{
+          textDecoration: "none",
+          color: "black",
+          fontSize: "18px",
+        }}
+      >
+        {" "}
+        <p>Click here for more ...</p>
+      </Link>
     </div>
   );
 }
