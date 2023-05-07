052a5fb32de7368383f25447d1b19ac94f0f9c34
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-07 19:27:34 +0200

    add variables for work and education

---
+++ vite-app/src/components/About/Education.js
@@ -0,0 +1,24 @@
+export const educationMileStone1 = "Educational leave,Full Stack Developer";
+export const educationMileStone1Period = "05/2022  - now";
+export const educationMileStone2 = "Self study in programming during pandemic";
+export const educationMileStone3 = "University course Project Management, Graz";
+export const educationMileStone3Period = "10/2018 – 12/2018";
+export const educationMileStone4 = "Self study in Photography";
+export const educationMileStone4Period = "2016";
+export const educationMileStone5 = "Bachelor of Arts, Educational Science";
+export const educationMileStone5Period = "10/2011 - 12/2015";
+
+export const experienceMileStone1 = "Educational leave,Full Stack Developer";
+export const experienceMileStone1Period = "05/2022  - now";
+export const experienceMileStone2 = "Sales assistant, K&Ö";
+export const experienceMileStone2Period = "02/2019 - 05/2022";
+export const experienceMileStone3 = "Work abroad (Germany) as Babyphotographer";
+export const experienceMileStone3Period = "12/2017 - 06/2018";
+export const experienceMileStone4 = "Social Worker, Vienna";
+export const experienceMileStone4Period = "05/2016 – 04/2017";
+export const experienceMileStone5 = "Voluntary Work:";
+export const experienceMileStone5Period = "Lebenshilfe Feldbach 06 - 08/2011";
+export const studentjobs =
+  "Studentjobs: GIP (Generation in Partnership), Marketing Consultant Joy Sportswear GmbH, SIQ(Sport-Integration-Qualification) Caritas";
+export const studentjobsPeriod =
+  "09/2012 - 06/2014, 11/2014 - 12/2015, 2011-2012 ";
