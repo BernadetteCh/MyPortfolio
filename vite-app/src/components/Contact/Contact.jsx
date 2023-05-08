323352a24f06896e67306b0c7af79fbf4f8c6950
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-08 17:09:23 +0200

    import icons and create contact component

--- vite-app/src/components/Contact/Contact.jsx
+++ vite-app/src/components/Contact/Contact.jsx
@@ -1,6 +1,10 @@
 import Headline from "../Headline";
 import "./Contact.css";
 import contactHeadline from "../../img/contact.png";
+import linkedInIcon from "../../img/linkedin.png";
+import githubIcon from "../../img/githubicon.png";
+import discordIcon from "../../img/discordIcon.png";
+import { Link } from "react-router-dom";
 
 function Contact() {
   return (
@@ -8,6 +12,35 @@ function Contact() {
       <Headline header={"Contact "} />
       <div>
         <img src={contactHeadline} className="headline-contact"></img>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <Link
+          to="mailto:b.christiner93@gmail.com"
+          style={{
+            textDecoration: "none",
+            display: "block",
+            margin: "0px auto",
+            fontSize: "20px",
+          }}
+        >
+          <button className="contactViaMail">Contact Me</button>
+        </Link>
+
+        <p className="emailaddress">b.christiner93@gmail.com</p>
+      </div>
+      <div style={{ display: "block", textAlign: "center" }}>
+        <Link to="https://at.linkedin.com/" target="_blank">
+          <img src={linkedInIcon} className="contact-icon"></img>
+        </Link>
+        <Link to="https://github.com/BernadetteCh" target="_blank">
+          <img src={githubIcon} className="contact-icon"></img>
+        </Link>
+        <Link
+          to="https://discordapp.com/users/949019145968705669"
+          target="_blank"
+        >
+          <img src={discordIcon} className="contact-icon"></img>
+        </Link>
       </div>
     </div>
   );
