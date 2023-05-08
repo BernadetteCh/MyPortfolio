7bf18b1a80bee76339865185553139ead0ba3064
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-08 14:40:43 +0200

    create component to display each skill in a column

---
+++ vite-app/src/components/Skills/SkillIcon.jsx
@@ -0,0 +1,12 @@
+import "../../components/Skills/Skills.css";
+
+function SkillIcons({ icon, description }) {
+  return (
+    <div className=" col-4 col-md-3 skillicon-container">
+      <img src={icon} className="img-fluid skillicon"></img>
+      <div className="description">{description}</div>
+    </div>
+  );
+}
+
+export default SkillIcons;
