2f876f1663785ff8b1438aae4677630cde22d4c1
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-08 16:28:51 +0200

    add img of headline

--- vite-app/src/components/Contact/Contact.jsx
+++ vite-app/src/components/Contact/Contact.jsx
@@ -1,5 +1,16 @@
+import Headline from "../Headline";
+import "./Contact.css";
+import contactHeadline from "../../img/contact.png";
+
 function Contact() {
-  return <div>Hi from Contact</div>;
+  return (
+    <div>
+      <Headline header={"Contact "} />
+      <div>
+        <img src={contactHeadline} className="headline-contact"></img>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
