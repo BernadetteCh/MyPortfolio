63a06d8c24b4d8dde6b1d2711b5ac12858db93cd
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-09 22:39:05 +0200

    remove router-dom to create one page with anchorlinks

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -7,20 +7,27 @@ import Projects from "./components/Projects/Projects.jsx";
 import Skills from "./components/Skills/Skills.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <div>
       <NavigationBar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/aboutme" element={<AboutMe />}></Route>
-        <Route path="/projects" element={<Projects />}></Route>
-        <Route path="/skills" element={<Skills />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
-    </BrowserRouter>
+      <section id="home">
+        <Home />
+      </section>
+      <section id="aboutme">
+        <AboutMe />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="skills">
+        <Skills />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+    </div>
   );
 }
 
