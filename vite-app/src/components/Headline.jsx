e6424bb6a85cf709eb5ae5a7b3457375def97441
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-09 22:39:15 +0200

    add margin

--- vite-app/src/components/Headline.jsx
+++ vite-app/src/components/Headline.jsx
@@ -2,7 +2,7 @@ import "../../src/components/Headline.css";
 
 function Headline({ header }) {
   return (
-    <h2 style={{ fontSize: "60px" }}>
+    <h2 style={{ fontSize: "60px", marginTop: "100px" }}>
       {"<"}
       {header}
       {"/>"}
