6922e8e83d7923c1ee4000fd94459c890e0cd4d2
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-10 14:26:58 +0200

    change width of columns

--- vite-app/src/components/Columns/CVHeader.jsx
+++ vite-app/src/components/Columns/CVHeader.jsx
@@ -1,17 +1,17 @@
 function CVHeader({ education, experience }) {
   return (
     <div className="container text-center">
-      <div className="row justify-content-center">
-        <div className="col-6">
+      <div className="row justify-content-evenly">
+        <div className="col-2">
           <img
             src={education}
-            style={{ margin: "0px", maxWidth: "800px" }}
+            style={{ margin: "0px", maxWidth: "200px" }}
           ></img>
         </div>
-        <div className="col-6">
+        <div className="col-2">
           <img
             src={experience}
-            style={{ margin: "0px", maxWidth: "800px" }}
+            style={{ margin: "0px", maxWidth: "200px" }}
           ></img>
         </div>
       </div>
