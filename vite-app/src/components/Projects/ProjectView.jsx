eb5d13dc63e11226b6dba9c8aa9b52064f6ec895
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-10 20:05:18 +0200

    remove lg from class

--- vite-app/src/components/Projects/ProjectView.jsx
+++ vite-app/src/components/Projects/ProjectView.jsx
@@ -2,9 +2,9 @@ import "./Projects.css";
 
 function ProjectView({ link, image, projecttitle, technologies }) {
   return (
-    <div className="col-lg-4">
+    <div className="col-4">
       <a href={link} target="_blank" alt={projecttitle}>
-        <img src={image} className="project-image img-fluid"></img>
+        <img src={image} className="project-image "></img>
       </a>
       <div className="project-title">
         <h3>{projecttitle}</h3>
