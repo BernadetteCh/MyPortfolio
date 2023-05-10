67e30ae64dbaac6b4e221339c63149c3a5c62233
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-10 22:21:43 +0200

    create data file with information about projects

---
+++ vite-app/src/components/Projects/Projectsdata.js
@@ -0,0 +1,77 @@
+import robocat from "../../img/projects/robocat.png";
+import TicTacToe from "../../img/projects/TicTacToeGame.png";
+import reactweatherapp from "../../img/weatherapp.png";
+import dictionaryapp from "../../img/dictionaryapp.png";
+import mars from "../../img/projects/mars.png";
+import myemployeesdata from "../../img/employeemadness.png";
+import weatherapp from "../../img/projects/weatherapp2.png";
+import calculatorapp from "../../img/projects/calculator.png";
+import italyapp from "../../img/projects/learnItalianApp.png";
+
+const imgtitle = "click to view project";
+
+export const projectsData = [
+  {
+    name: robocat,
+    title: imgtitle,
+    projecttitle: "RoboCats",
+    technologies: "Springboot, Java",
+    link: "https://github.com/BernadetteCh/RoboCats",
+  },
+  {
+    name: TicTacToe,
+    title: imgtitle,
+    projecttitle: "TicTacToe",
+    technologies: "html, css and javascript, bootstrap",
+    link: "https://ubiquitous-chebakia-24042d.netlify.app/",
+  },
+  {
+    name: reactweatherapp,
+    title: imgtitle,
+    projecttitle: "React_WeatherApp",
+    technologies: "React, Axios, Bootstrap, Restful API",
+    link: "https://optimistic-curran-8bd91f.netlify.app/",
+  },
+  {
+    name: dictionaryapp,
+    title: imgtitle,
+    projecttitle: "DictionaryApp",
+    technologies: "React, Axios, Bootstrap, Restful API",
+    link: "https://frosty-lumiere-3bb161.netlify.app/",
+  },
+  {
+    name: mars,
+    title: imgtitle,
+    projecttitle: "MarsRover",
+    technologies: "Java, Postgresql, PostgreSQL",
+    link: "#",
+  },
+  {
+    name: myemployeesdata,
+    title: imgtitle,
+    projecttitle: "My EmployeesData",
+    technologies: "MongoDB, ExpressJS, React",
+    link: "https://github.com/BernadetteCh/my-employees-data/",
+  },
+  {
+    name: weatherapp,
+    title: imgtitle,
+    projecttitle: "WeatherApp",
+    technologies: "HTML, CSS and JavaScript, Bootstrap",
+    link: "https://xenodochial-noether-025d51.netlify.app/",
+  },
+  {
+    name: calculatorapp,
+    title: imgtitle,
+    projecttitle: "CalculatorApp",
+    technologies: "HTML, CSS and JavaScript, Bootstrap",
+    link: "https://goofy-goldstine-6e0330.netlify.app/",
+  },
+  {
+    name: italyapp,
+    title: imgtitle,
+    projecttitle: "Learn_ItalianApp",
+    technologies: "HTML, CSS and JavaScript, Bootstrap (first project)",
+    link: "https://confident-thompson-888a16.netlify.app/",
+  },
+];
