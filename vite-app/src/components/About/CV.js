4f5550af04d93261a024ec65310ed9f9cee51f59
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-11 18:13:38 +0200

    create new file for cv data

---
+++ vite-app/src/components/About/CV.js
@@ -0,0 +1,68 @@
+export const educationMileStone1 = "Educational leave,Full Stack Developer";
+export const educationMileStone1Period = "05/2022  - now";
+export const educationMileStone2 = "Self study in programming during pandemic";
+export const educationMileStone2Period = "with SheCodes workshop";
+export const educationMileStone3 = "University course Project Management, Graz";
+export const educationMileStone3Period = "10/2018 – 12/2018";
+const educationMileStone4 = "Self study in Photography";
+const educationMileStone4Period = "2016";
+const educationMileStone5 = "Bachelor of Arts, Educational Science";
+const educationMileStone5Period = "10/2011 - 12/2015";
+
+const experienceMileStone1 = "Educational leave,Full Stack Developer";
+const experienceMileStone1Period = "05/2022  - now";
+const experienceMileStone2 = "Sales assistant, K&Ö";
+const experienceMileStone2Period = "02/2019 - 05/2022";
+const experienceMileStone3 = "Work abroad (Germany) as Babyphotographer";
+const experienceMileStone3Period = "12/2017 - 06/2018";
+const experienceMileStone4 = "Social Worker";
+const experienceMileStone4Period = "05/2016 – 04/2017";
+const experienceMileStone5 = "Voluntary Work:";
+const experienceMileStone5Period = "Lebenshilfe Feldbach 06 - 08/2011";
+const studentjobs = `Studentjobs: GIP (Generation in Partnership), Marketing Consultant Joy Sportswear GmbH, SIQ(Sport-Integration-Qualification) Caritas`;
+const studentjobsPeriod = "09/2012 - 06/2014, 11/2014 - 12/2015, 2011-2012 ";
+
+export const curriculumVitea = [
+  {
+    educationHeader: educationMileStone1,
+    educationSubheader: educationMileStone1Period,
+    experienceHeader: experienceMileStone1,
+    experienceSubheader: experienceMileStone1Period,
+  },
+  {
+    educationHeader: educationMileStone2,
+    educationSubheader: educationMileStone2Period,
+    experienceHeader: experienceMileStone2,
+    experienceSubheader: experienceMileStone2Period,
+  },
+  {
+    educationHeader: educationMileStone3,
+    educationSubheader: educationMileStone3Period,
+    experienceHeader: "",
+    experienceSubheader: "",
+  },
+  {
+    educationHeader: "",
+    educationSubheader: "",
+    experienceHeader: experienceMileStone3,
+    experienceSubheader: experienceMileStone3Period,
+  },
+  {
+    educationHeader: educationMileStone4,
+    educationSubheader: educationMileStone4Period,
+    experienceHeader: experienceMileStone4,
+    experienceSubheader: experienceMileStone4Period,
+  },
+  {
+    educationHeader: educationMileStone5,
+    educationSubheader: educationMileStone5Period,
+    experienceHeader: studentjobs,
+    experienceSubheader: studentjobsPeriod,
+  },
+  {
+    educationHeader: "",
+    educationSubheader: "",
+    experienceHeader: experienceMileStone5,
+    experienceSubheader: experienceMileStone5,
+  },
+];
