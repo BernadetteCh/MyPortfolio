4a6497fb0caee58802f7e711f4e6aa8d376d8c93
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-15 21:27:59 +0200

    edit content of selfintroduction text

--- vite-app/src/components/About/SelfIntroduction.js
+++ vite-app/src/components/About/SelfIntroduction.js
@@ -1,4 +1,4 @@
-export const selfIntroduction1 = `Hallo, my name is Bernadette and I am a fullstack software developer
+/*export const selfIntroduction1 = `Hallo, my name is Bernadette and I am a fullstack software developer
           from Austria. I am always interested in learning new things and
           educate myself. I started learning to code during the pandemic by
           self-study and online workshop of SheCodes, which provides "hands on"
@@ -15,4 +15,22 @@ export const selfIntroduction2 = ` Starting exploring a new world was quite exci
 export const selfIntroduction3 = `  I would say my biggest strength is my courage and one weakness of mine
           is that I am also a detail oriented person so it can happen that I can
           get lost in details. If you like my work or if you would like to give
-          feedback, I would be very happy to receive a message.`;
+          feedback, I would be very happy to receive a message.`;*/
+
+export const selfIntroduction1 = `Before I changed careers, I mainly used my laptop to join gymnastics videos and to cook from YouTube videos.
+A dark time in my life, but "In the darkest night, a spark ignites, showing you a path so bright". The little spark
+came out of my laptop during the pandemic when I tried programming and failed immediately afterwards.
+I left my fear of computers and computer science classes at school behind me and continued to study.
+I immersed myself in a new world in which I discovered something new every time, could learn and had fun
+doing it.`;
+
+export const selfIntroduction2 = `For more than a year now I've been learning to program or should I say googling. Since it is a very time-
+consuming training, I made
+myself completely dependent on this search engine and my laptop.`;
+
+export const selfIntroduction3 = `I like working in teams and appreciate a respectful and harmonious relationship with each other. I also enjoy
+having time to myself from time to time and challenging myself by implementing my own ideas.
+I work conscientiously and detail-oriented. Furthermore, my strength lies in not letting my programming skills
+be negatively affected and not giving up, even in stressful situations.
+
+Today I can proudly say: Hello World ! `;
