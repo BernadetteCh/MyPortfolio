fc0d98b135ea91e4a77c9db2a4cdd9377703bbb6
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-17 09:03:31 +0200

    Change introduction text

--- vite-app/src/components/About/SelfIntroduction.js
+++ vite-app/src/components/About/SelfIntroduction.js
@@ -17,10 +17,9 @@ export const selfIntroduction3 = `  I would say my biggest strength is my courag
           get lost in details. If you like my work or if you would like to give
           feedback, I would be very happy to receive a message.`;*/
 
-export const selfIntroduction1 = `Before I changed careers, I mainly used my laptop to join gymnastics videos and to cook from YouTube videos.
-A dark time in my life, but "In the darkest night, a spark ignites, showing you a path so bright". The little spark
-came out of my laptop during the pandemic when I tried programming and failed immediately afterwards.
-I left my fear of computers and computer science classes at school behind me and continued to study.
+export const selfIntroduction1 = `Before I changed careers, honestly, I  mainly used my laptop to watch Youtube videos.
+During the pandemic I started trying programming and failed immediately afterwards.
+I left my fear of computers behind me and continued to study.
 I immersed myself in a new world in which I discovered something new every time, could learn and had fun
 doing it.`;
 
