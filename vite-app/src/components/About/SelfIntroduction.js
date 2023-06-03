169637df79d35bd5bd95036dae7b7615c5a0f271
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-04 00:09:11 +0200

    Remove comments

--- vite-app/src/components/About/SelfIntroduction.js
+++ vite-app/src/components/About/SelfIntroduction.js
@@ -1,35 +1,16 @@
-/*export const selfIntroduction1 = `Hallo, my name is Bernadette and I am a fullstack software developer
-          from Austria. I am always interested in learning new things and
-          educate myself. I started learning to code during the pandemic by
-          self-study and online workshop of SheCodes, which provides "hands on"
-          front-end developer coding workshops.`;
-export const selfIntroduction2 = ` Starting exploring a new world was quite exciting so it came that I
-          spent more than a year on a coding school in Vienna to built up an
-          expertise in different technologies and how to put my own project
-          ideas to practice. Besides coding I like to spend my time in the
-          nature because it clears my head and it's like balm for the soul . I
-          also like to live out my creativity with hyper realistic drawing and
-          photography. So it can happen you find me taking pictures on top of a
-          mountain or in a restless city, as photography has been a hobby of
-          mine for a long time.`;
-export const selfIntroduction3 = `  I would say my biggest strength is my courage and one weakness of mine
-          is that I am also a detail oriented person so it can happen that I can
-          get lost in details. If you like my work or if you would like to give
-          feedback, I would be very happy to receive a message.`;*/
+export const selfIntroduction1 = `I like working in teams and appreciate a respectful and harmonious relationship with each other. I also enjoy
+having time to myself from time to time and challenging myself by implementing my own ideas.
+I approach work with a conscientious and detail-oriented mindset. Moreover, I take pride in my ability to maintain the quality of my programming skills despite any challenges,
+and I remain determined and resilient even in the face of stressful situations. After countless challenges, failures but also successes I can proudly say
 
-export const selfIntroduction1 = `Before I changed careers, honestly, I  mainly used my laptop to watch Youtube videos.
+Hello World !`;
+
+export const selfIntroduction2 = `Before I changed careers, honestly, I  mainly used my laptop to watch Youtube videos.
 During the pandemic I started trying programming and failed immediately afterwards.
 I left my fear of computers behind me and continued to study.
 I immersed myself in a new world in which I discovered something new every time, could learn and had fun
 doing it.`;
 
-export const selfIntroduction2 = `For more than a year now I've been learning to program or should I say googling. Since it is a very time-
+export const selfIntroduction3 = `For more than a year now I've been learning to program or should I say googling. Since it is a very time-
 consuming training, I made
 myself completely dependent on this search engine and my laptop.`;
-
-export const selfIntroduction3 = `I like working in teams and appreciate a respectful and harmonious relationship with each other. I also enjoy
-having time to myself from time to time and challenging myself by implementing my own ideas.
-I approach work with a conscientious and detail-oriented mindset. Moreover, I take pride in my ability to maintain the quality of my programming skills despite any challenges,
-and I remain determined and resilient even in the face of stressful situations. After countless challenges, failures but also successes I can proudly say
-
-Hello World ! `;
