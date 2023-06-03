d5ed77ebfa69ef146f6c871e58c11075f493047b
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-04 00:14:48 +0200

    Create new componnent

---
+++ vite-app/src/components/Skills/Certificates.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import shecodesreact from "../../img/shecodesreact.png";
+import shecodesfrontend from "../../img/shecodesfrontend.png";
+import skills from "../../img/skills.png";
+
+function Certificates() {
+  return (
+    <div>
+      <img
+        src={shecodesfrontend}
+        className="img-fluid shecodescertificate"
+        style={{
+          boxShadow:
+            "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
+          margin: "10px",
+        }}
+      ></img>
+      <img
+        src={shecodesreact}
+        className="img-fluid shecodescertificate"
+        style={{
+          boxShadow:
+            "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
+        }}
+      ></img>
+    </div>
+  );
+}
+
+export default Certificates;
