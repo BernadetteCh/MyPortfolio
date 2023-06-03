feb187cee5cb9a31600f7090a77540cafb20cc4c
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-04 00:08:47 +0200

    Create new component

---
+++ vite-app/src/components/About/MentorReference.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function MentorReference() {
+  return (
+    <div>
+      <h3 style={{ fontWeight: "bold" }}>References</h3>
+      <p>Mentors who supported me during my coding journey</p>
+      <p>
+        <span className="mentor pe-4">Emad Easa Senior Developer:</span>
+        <span>
+          ,,With her Growth mindset, embraced challenges and dedicated herself
+          to becoming <br />a skilled full-stack developer, while her altruistic
+          nature and active involvement in social activities reflected <br />
+          her commitment to helping others. "
+        </span>
+      </p>
+      <p>
+        <span className="mentor">Guillermo Herrero Senior Developer</span>
+      </p>
+    </div>
+  );
+}
+
+export default MentorReference;
