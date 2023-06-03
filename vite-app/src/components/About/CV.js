2c94ce6fb076c2087a757044111e67d7406b90ba
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-04 00:08:28 +0200

    Edit content

--- vite-app/src/components/About/CV.js
+++ vite-app/src/components/About/CV.js
@@ -1,11 +1,11 @@
 export const educationMileStone1 = "Educational leave,Full Stack Developer";
 export const educationMileStone1Period = "05/2022  - now";
-export const educationMileStone2 = "Self study in programming during pandemic";
-export const educationMileStone2Period = "with SheCodes workshop";
+export const educationMileStone2 = "SheCodes ,,Hands on'' Coding Workshops ";
+export const educationMileStone2Period = "09/2021 – 01/2022";
 export const educationMileStone3 = "University course Project Management, Graz";
 export const educationMileStone3Period = "10/2018 – 12/2018";
-const educationMileStone4 = "Self study in Photography";
-const educationMileStone4Period = "2016";
+const educationMileStone4 = "";
+const educationMileStone4Period = "";
 const educationMileStone5 = "Bachelor of Arts, Educational Science";
 const educationMileStone5Period = "10/2011 - 12/2015";
 
@@ -13,9 +13,9 @@ const experienceMileStone1 = "Educational leave,Full Stack Developer";
 const experienceMileStone1Period = "05/2022  - now";
 const experienceMileStone2 = "Sales assistant, K&Ö";
 const experienceMileStone2Period = "02/2019 - 05/2022";
-const experienceMileStone3 = "Work abroad (Germany) as Babyphotographer";
+const experienceMileStone3 = "BabySmile GmbH";
 const experienceMileStone3Period = "12/2017 - 06/2018";
-const experienceMileStone4 = "Social Worker";
+const experienceMileStone4 = "Pedagogue";
 const experienceMileStone4Period = "05/2016 – 04/2017";
 const experienceMileStone5 = "Voluntary Work:";
 const experienceMileStone5Period = "Lebenshilfe Feldbach 06 - 08/2011";
