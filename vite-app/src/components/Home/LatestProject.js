d8fa11259b2ad6875776cc7ed33d0577e8e9eeee
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-04 00:11:45 +0200

    Create new data file

---
+++ vite-app/src/components/Home/LatestProject.js
@@ -0,0 +1,21 @@
+import weatherapp from "../../img/weatherapp.png";
+import dictionaryapp from "../../img/dictionaryapp.png";
+import employeemadness from "../../img/employeemadness.png";
+
+export const latestProjects = [
+  {
+    name: "Weatherapp",
+    link: "https://optimistic-curran-8bd91f.netlify.app/",
+    imgSrc: weatherapp,
+  },
+  {
+    name: "Dictionaryapp",
+    link: "https://frosty-lumiere-3bb161.netlify.app/",
+    imgSrc: dictionaryapp,
+  },
+  {
+    name: "EmployeeMadness",
+    link: "https://github.com/BernadetteCh/my-employees-data",
+    imgSrc: employeemadness,
+  },
+];
