c98c45fdd93313912cd178034124adf4e0b60409
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-04 00:14:30 +0200

    Add box shadow

--- vite-app/src/components/Projects/ProjectView.jsx
+++ vite-app/src/components/Projects/ProjectView.jsx
@@ -4,7 +4,13 @@ function ProjectView({ link, title, image, projecttitle, technologies }) {
   return (
     <div className="col-4">
       <a href={link} target="_blank" alt={projecttitle} title={title}>
-        <img src={image} className="project-image "></img>
+        <img
+          src={image}
+          className="project-image"
+          style={{
+            boxShadow: "rgb(38, 57, 77) 0px 20px 30px -10px",
+          }}
+        ></img>
       </a>
       <div className="project-title">
         <h3>{projecttitle}</h3>
