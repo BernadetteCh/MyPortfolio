990373b9ad8d45ba38ae690ea13053bb7d9177f6
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-04 12:33:03 +0200

    Remove comment

--- vite-app/src/components/Home/LatestProjects.jsx
+++ vite-app/src/components/Home/LatestProjects.jsx
@@ -7,9 +7,12 @@ function LatestProjects({ project }) {
         {" "}
         <img
           src={project.imgSrc}
+          // style={{
+          //   boxShadow:
+          //     "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px",
+          // }}
           style={{
-            boxShadow:
-              "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px",
+            boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
           }}
           alt={project.name}
         ></img>
