477440680503756461fa29fde837136d96880a4e
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-08 18:57:52 +0200

    Import german cv

--- vite-app/src/components/About/AboutMe.jsx
+++ vite-app/src/components/About/AboutMe.jsx
@@ -15,6 +15,7 @@ import { curriculumVitea } from "./CV";
 import leaf from "../../img/leaf.png";
 import BCHCV from "../../../src/CV_BCH.pdf";
 import CVEngl from "../../../src/CV-engl.pdf";
+import CVGerman from "../../src/CV-german.pdf";
 import MentorReference from "./MentorReference";
 
 function AboutMe() {
@@ -67,7 +68,7 @@ function AboutMe() {
           );
         })}
       </div>
-      <a href={CVEngl} className="download-cv" target="_blank">
+      <a href={CVGerman} className="download-cv" target="_blank">
         Download CV
       </a>
       {openModal && (
