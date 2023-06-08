ff1e11f5e2279ebbd75f8b5b538d81c2a06c06a7
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-08 18:01:06 +0200

    Remove import statement

--- vite-app/src/components/Projects/Projects.jsx
+++ vite-app/src/components/Projects/Projects.jsx
@@ -1,15 +1,6 @@
 import "./Projects.css";
 import Headline from "../../components/Headline.jsx";
 import ProjectView from "../Projects/ProjectView.jsx";
-import robocat from "../../img/projects/robocat.png";
-import tictactoe from "../../img/projects/TicTacToeGame.png";
-import reactweatherapp from "../../img/weatherapp.png";
-import weatherapp from "../../img/projects/weatherapp2.png";
-import dictionaryapp from "../../img/dictionaryapp.png";
-import myemployeesdata from "../../img/employeemadness.png";
-import italyapp from "../../img/projects/learnItalianApp.png";
-import calculatorapp from "../../img/projects/calculator.png";
-import mars from "../../img/projects/mars.png";
 import { projectsData } from "./Projectsdata";
 
 function Projects() {
